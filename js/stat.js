8fb07e50b89b317a21a400442ffe9ab67aa5658e
Boris davydenko <brsdv@yandex.ru>
2019-06-07 12:00:42 +0300

    Реализация задачи - Учебный проект Холст

---
+++ js/stat.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 20; // Отступы от текста
+var COLUMN_GAP = 50; // Расстояние между колонками
+var BAR_WIDTH = 40; // Ширина колонки
+var BAR_HEIGHT = -150; // Высота колонки (с отрицательным числом чтобы колонка стремилась вверх, а не вниз)
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = 'bold 16px PT Mono';
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < names.length; i++) {
+    var intNumber = Math.round(times[i]);
+
+    if (names[i].indexOf('Вы') === 0) {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      var randomNumber = Math.random();
+      var color = 'rgba(0, 0, 255, ' + randomNumber + ')';
+      ctx.fillStyle = color;
+    }
+
+    ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_WIDTH + COLUMN_GAP) * i, CLOUD_HEIGHT - FONT_GAP, BAR_WIDTH, (intNumber * BAR_HEIGHT) / maxTime);
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], CLOUD_X + BAR_WIDTH + (BAR_WIDTH + COLUMN_GAP) * i, CLOUD_HEIGHT);
+    ctx.fillText(intNumber, CLOUD_X + BAR_WIDTH + (BAR_WIDTH + COLUMN_GAP) * i, CLOUD_HEIGHT - FONT_GAP - Math.abs(intNumber * BAR_HEIGHT / maxTime) - GAP);
+  }
+};
