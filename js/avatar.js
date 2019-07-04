d4e95066bd7e6d5b4282e292872582e53cb7a9dd
Boris Davydenko <davydenko.b.i@ulmart.ru>
2019-07-04 21:55:30 +0300

    Выполнение задания - выкладываем фото

---
+++ js/avatar.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['jpg', 'jpeg', 'png', 'gif'];
+
+  var fileChooser = document.querySelector('.upload input[type=file]');
+  var preview = document.querySelector('.setup-user-pic');
+
+  fileChooser.addEventListener('change', function () {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (element) {
+      return fileName.endsWith(element);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+        console.log(reader);
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+
+})();
